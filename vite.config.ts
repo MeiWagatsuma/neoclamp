893ce9636968aef6e50c6672a5d3f14e16594015
MeiWagatsuma <wagamei256@gmail.com>
2022-11-08 10:08:37 +0900

    Added vite-plugin-checker to package
    Errors are displayed in the browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        dev: {
+          logLevel: ['error']
+        }
+      }
+    })
+  ],
   build: {
-    sourcemap: false,
-  },
-});
+    sourcemap: false
+  }
+})

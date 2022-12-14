686c9a38099d6855ceead93fcf99ef93c39d9666
MeiWagatsuma <wagamei256@gmail.com>
2022-12-14 10:38:18 +0900

    Refactored backgroundColor and Text size.
    Background color was made transparent.
    Text height was made unnecessary.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,10 +1,12 @@
+import LinePath from './LinePath'
+
 interface GraphProps {
-  backgroundColor: string
   width: number
   height: number
 }
-const TEXT_WIDTH = 20
-function Graph({ backgroundColor, width, height }: GraphProps): JSX.Element {
+
+function Graph({ width, height }: GraphProps): JSX.Element {
+  const TEXT_WIDTH = 20
   const TEXT_HEIGHT = 20
   const textAreaWidth = TEXT_WIDTH * 4
   const textAreaHeigh = TEXT_HEIGHT * 2
@@ -17,20 +19,20 @@ function Graph({ backgroundColor, width, height }: GraphProps): JSX.Element {
         <rect width="400" height="300" fill="#b6c0e7" />
         <path
           className="axis-line"
-          strokeWidth="30"
+          strokeWidth="3"
           d={`
             M ${textAreaWidth} 0
             l 0 ${graphY}
             l ${graphX} 0
           `}
           stroke="#000"
-          fill={backgroundColor}
+          fill="none"
         />
         <path
           strokeWidth="3"
           stroke="blue"
           d={`
-            M ${textAreaWidth} 0
+            M ${textAreaWidth} ${graphY}
             ${LinePath({ x: [100, 200, 300], y: [400, 500, 300] })}
           `}
           fill="none"

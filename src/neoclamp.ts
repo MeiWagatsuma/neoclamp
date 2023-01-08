1fbe2c9488255bfcc5eeec1357da8fbab5269499
MeiWagatsuma <wagamei256@gmail.com>
2023-01-08 19:44:19 +0900

    Modularized neoclamp functions

--- src/neoclamp.ts
+++ src/neoclamp.ts
@@ -1,17 +1,44 @@
-export function neoclamp(
+export function computeSlope(
+  startPoint: number,
+  startPixel: number,
+  endPoint: number,
+  endPixel: number
+): number {
+  return (endPixel - startPixel) / (endPoint - startPoint)
+}
+
+export function computeDiff(
+  startPoint: number,
+  startPixel: number,
+  slope: number
+): number {
+  return startPixel - slope * startPoint
+}
+
+export function formatCssClamp(
+  min: number,
+  slope: number,
+  diff: number,
+  max: number
+): string {
+  return `clamp(${min}px, ${slope * 100}vw${
+    diff === 0 ? '' : ` + ${diff}px`
+  }, ${max}px)`
+}
+
+export default function neoclampCss(
   startPoint: number,
   startPixel: number,
   endPoint: number,
   endPixel: number
 ): string {
-  const slope = (endPixel - startPixel) / (endPoint - startPoint)
-  if (isNaN(slope)) {
-    console.warn(
-      `Invalid formula: (${endPixel} - ${startPixel}) / (${endPoint} - ${startPoint})`
-    )
+  const slope = computeSlope(startPoint, startPixel, endPoint, endPixel)
+  if (startPoint === endPoint) {
+    console.error(`startPoint and endPoint must be set to different values.`)
   }
-  const interpolation = startPixel - slope * startPoint
-  return `clamp(${Math.min(startPixel, endPixel)}px, ${
-    slope * 100
-  }vw + ${interpolation}px, ${Math.max(startPixel, endPixel)}px)`
+
+  const diff = computeDiff(startPoint, startPixel, slope)
+  const min = Math.min(startPixel, endPixel)
+  const max = Math.max(startPixel, endPixel)
+  return formatCssClamp(min, slope, diff, max)
 }

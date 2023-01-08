5dc37ae6d5383110dc79d6ec4422a3ea2caaef65
MeiWagatsuma <wagamei256@gmail.com>
2023-01-08 21:57:04 +0900

    Added longNeoclamp module

---
+++ src/longNeoclamp.ts
@@ -0,0 +1,26 @@
+import neoclamp from './neoclamp'
+
+export default function longNeoclamp(...arr: number[]): string {
+  if (arr.length % 2 === 1) {
+    console.warn('Arguments must be even numbers')
+    return ''
+  }
+  const clampList: string[] = []
+  console.log('length: ', arr.length)
+  for (let i = 3; i <= arr.length; i += 2) {
+    console.log('i: ', i)
+
+    const diff = i === 3 ? 0 : arr[i - 2]
+    console.log('diff: ', diff)
+    const clamp = neoclamp(
+      arr[i - 3],
+      arr[i - 2] - diff,
+      arr[i - 1],
+      arr[i] - diff
+    )
+    console.log(arr[i - 3], arr[i - 2], arr[i - 1], arr[i] - arr[i - 2])
+    console.log(clamp)
+    clampList.push(clamp)
+  }
+  return `calc(${clampList.join(' + ')})`
+}

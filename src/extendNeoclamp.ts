8cb60f3c0e9f770a836f8efc8f1268bbacbd245f
MeiWagatsuma <wagamei256@gmail.com>
2023-01-09 15:25:50 +0900

    Delete console.log.

--- src/extendNeoclamp.ts
+++ src/extendNeoclamp.ts
@@ -1,26 +1,29 @@
 import neoclamp from './neoclamp'
 
-export default function extendNeoclamp(...arr: number[]): string {
+export default function extendNeoclamp(
+  startPoint: number,
+  startPixel: number,
+  endPoint: number,
+  endPixel: number,
+  ...argArray: number[]
+): string {
+  const arr = [startPoint, startPixel, endPoint, endPixel, ...argArray]
   if (arr.length % 2 === 1) {
     console.warn('Arguments must be even numbers')
     return ''
   }
-  const clampList: string[] = []
-  console.log('length: ', arr.length)
-  for (let i = 3; i <= arr.length; i += 2) {
-    console.log('i: ', i)
 
+  const clampList: string[] = []
+  for (let i = 3; i <= arr.length; i += 2) {
     const diff = i === 3 ? 0 : arr[i - 2]
-    console.log('diff: ', diff)
     const clamp = neoclamp(
       arr[i - 3],
       arr[i - 2] - diff,
       arr[i - 1],
       arr[i] - diff
     )
-    console.log(arr[i - 3], arr[i - 2], arr[i - 1], arr[i] - arr[i - 2])
-    console.log(clamp)
     clampList.push(clamp)
   }
+
   return `calc(${clampList.join(' + ')})`
 }
